26019628404ea79945adc435eb1ab1ebc8ec9b61
Ana <anappn19@gmail.com>
2019-08-26 19:21:45 -0300

    Otimizando o código

--- mybot/bot.py
+++ mybot/bot.py
@@ -1,15 +1,20 @@
 from chatterbot.trainers import ListTrainer
 from chatterbot import ChatBot
 
-bot = ChatBot('test')
+bot = ChatBot('NiceBot')
 
 texto = open("conversa.txt",'r')
 texto = texto.readlines()
 trainer = ListTrainer(bot)
 trainer.train(texto)
 
+
 while True:
-    for i in texto:
-        quest = input("Você: ")
-        resposta = bot.get_response(quest)
-        print('Bot: ',resposta)
+    try:
+        print("Olá, meu nome é NiceBot estou aqui para te ajudar o que você gostaria de saber?")
+        print("Para sair do chat, pressione Ctrl+c")
+        for i in texto:
+            quest = input("Você: ")
+            resposta = bot.get_response(quest)
+            print('Bot: ',resposta)
+    except(KeyboardInterrupt, EOFError, SystemExit):break
