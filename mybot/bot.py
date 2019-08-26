8a35c7b3af79a975b7213df4337038bba883a2b6
Ana <anappn19@gmail.com>
2019-08-25 22:10:36 -0300

    Colocando mensagens em arquivo

--- mybot/bot.py
+++ mybot/bot.py
@@ -1,13 +1,18 @@
 from chatterbot.trainers import ListTrainer
 from chatterbot import ChatBot
 
+
+
 bot = ChatBot('test')
 
-conversa = ['Oi','Olá','E aí, tudo bem?','tudo bem sim, e você?','Eu estou bem', 'Que bom!', 'O que você gosta de fazer?','Eu gosto de aprender a programar', 'Que linguagem de programação você mais gosta?', 'Eu gosto de python e gosto de Java','Quais filmes você mais gosta?']
+
+texto = open("conversa.txt",'r')
+texto = texto.readlines()
 trainer = ListTrainer(bot)
-trainer.train(conversa)
+trainer.train(texto)
 
 while True:
-    quest = input("Você: ")
-    resposta= bot.get_response(quest)
-    print('bot: ',resposta)
+    for i in texto:
+        quest = input("Você: ")
+        resposta = bot.get_response(quest)
+        print('Bot: ',resposta)
