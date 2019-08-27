5467f1325c707778b615ea9c2e98e11e94ac03b6
Ana <anappn19@gmail.com>
2019-08-27 16:28:16 -0300

    Pronto

--- mybot/bot.py
+++ mybot/bot.py
@@ -5,14 +5,14 @@ bot = ChatBot('NiceBot')
 
 texto = open("conversa.txt",'r')
 texto = texto.readlines()
+
 trainer = ListTrainer(bot)
 trainer.train(texto)
-
 
 while True:
     try:
         print("Olá, meu nome é NiceBot estou aqui para te ajudar o que você gostaria de saber?")
-        print("Para sair do chat, pressione Ctrl+c")
+        print("(Para sair do chat, pressione Ctrl+c)")
         for i in texto:
             quest = input("Você: ")
             resposta = bot.get_response(quest)
